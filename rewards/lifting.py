10fa5d27288c7cd851123ab031aa8a56e48c1fa8
gaoyuezhou <zhougy_99@berkeley.edu>
2023-10-31 16:43:15 -0400

    rename files for consistency

--- rewards/lifting.py
+++ rewards/lifting.py
@@ -1,20 +1,27 @@
 import numpy as np
 from franka_wrapper import FrankaWrapper
-from grasping import z_plane_adjust_batch
 
 # frame between robot and tag
 offset = np.array([1.09826201, -0.05144846999, 0.2276264])
 
+# LIMIT is very harsh (for determining death of agent); So let's make it loose
 JOINT_LIMIT_MIN = np.array(
         [-2.8773, -1.7428, -2.8773, -3.0018, -2.8773, 0.0025-np.pi/2, -2.8773, 0.0])
 JOINT_LIMIT_MAX = np.array(
-        [2.8773, 1.7428, 2.8773, -0.1398, 2.8773, 3.7325-np.pi/2, 2.8773, 0.085])
+        [2.8773, 1.7428, 2.8773, -0.1398, 2.8773, 3.7325-np.pi/2, 2.8773, 0.09])
 
 def coor_transform(tag_frame_pos):
     xt, yt, zt = tag_frame_pos
     x, y, z = -zt, -xt, yt
     franka_frame_pos = np.array([x, y, z]) + offset
     return franka_frame_pos
+
+def z_plane_adjust_batch(pos):
+    x, y, z = pos[:, 0], pos[:, 1], pos[:, 2]
+    new_pos = pos.copy()
+    z_base = (-0.000969 + 0.00182 * y + 0.00404 * x) / 0.02943
+    new_pos[:, 2] -= z_base
+    return new_pos
 
 def reward_function(paths):
     # paths has two keys: observations and actions
@@ -28,36 +35,38 @@ def reward_function(paths):
     gripper_dists = np.zeros((num_traj, horizon))
     goal_dists = np.zeros((num_traj, horizon))
     for i in range(num_traj):
-        tag_pos = paths["observations"][i, :,-6:-3] #both pos are transformed already
+        tag_pos = paths["observations"][i, :,-3:] #both pos are transformed 
         tag_pos = z_plane_adjust_batch(tag_pos)
-        goal_pos = paths["observations"][i, :,-3:]
         jointstates = paths["observations"][i, :, :8]
         ee_xyz = np.zeros((horizon, 3))
+        # import pdb;pdb.set_trace()
         for j, js in enumerate(jointstates):
-            if (js < JOINT_LIMIT_MAX).all() and (js > JOINT_LIMIT_MIN).all():
+            if (js <= JOINT_LIMIT_MAX).all() and (js >= JOINT_LIMIT_MIN).all():
+                #print(js)
                 ee_xyz[j] = franka.get_fk(js)
-                ee_xyz[j][2] -= 0.17
+                ee_xyz[j][2] -= 0.17 # for computing gripper dist with z_plane_adjusted tag_pos
             else:
                 ee_xyz[j] = -1
-        
-        gripper_dist = np.linalg.norm(tag_pos - ee_xyz, axis=1)
-        goal_dist = np.linalg.norm(tag_pos[:, :2] - goal_pos[:, :2], axis=1)
-        height_bonus = np.minimum(0.5, np.maximum(tag_pos[:, 2], 0) * 10) / 2
-        height_bonus = (gripper_dist < 0.1) * height_bonus 
-        rewards[i] = (1 - (gripper_dist * 2 + goal_dist)) * 0.9 + height_bonus 
-
-        ee_xyzs[i] = ee_xyz
-        gripper_dists[i] = gripper_dist
-        goal_dists[i] = goal_dist
-    paths['ee_xyz']  = ee_xyzs
-    paths['gripper_dist']  = gripper_dists
-    paths['goal_dist']  = goal_dists
+        # import pdb;pdb.set_trace()
+        gripper_dist = np.linalg.norm(tag_pos  - ee_xyz, axis=1)
+        height_bonus = np.minimum(0.5, np.maximum(tag_pos[:, 2], 0) * 10)
+        height_bonus = (gripper_dist < 0.1) * height_bonus
+        rewards[i] = np.minimum(1, (0.5 - gripper_dist + 0.07) + height_bonus)
+        # import pdb;pdb.set_trace()
+        # ee_xyzs[i] = ee_xyz
+        # gripper_dists[i] = gripper_dist
+    # print("first: ", paths["observations"][0, 0])
+    # print("last: ", paths["observations"][0, -1])
+    # paths['ee_xyz']  = ee_xyzs
+    # paths['gripper_dist']  = gripper_dists
     paths["rewards"] = rewards
     return paths
 
 def termination_function(paths):
+    # paths is a list of path objects for this function
     for path in paths:
         obs = path["observations"]
+        #print(obs[0:2], path["actions"][0])
         T = obs.shape[0]
         t = 0
         done = False
@@ -67,6 +76,8 @@ def termination_function(paths):
             done = not valid
             t = t + 1
             T = t if done else T
+        #np.set_printoptions(precision=4)
+        #print(path["observations"][:10, :8], T)
         path["observations"] = path["observations"][:T]
         path["actions"] = path["actions"][:T]
         path["rewards"] = path["rewards"][:T]
--- scripts/parse_lifting.py
+++ scripts/parse_lifting.py
@@ -7,16 +7,14 @@ import argparse
 from PIL import Image
 from tracking import get_markers_pos
 from camera_params import INTRINSICS_DICT, EXTRINSICS_DICT, CAMERA_IDS_DICT
-from gym import utils
-from gym.envs.mujoco import mujoco_env
 
 current_cameras = ['103422071965', '815412070238']
-MARKER_IDS = [4, 5, 10, 11, 2]
+MARKER_IDS = [4, 5, 10, 11]
 MARKER_SIZES = [0.05 for i in range(len(MARKER_IDS))]
 extrinsics = [EXTRINSICS_DICT[i] for i in current_cameras]
 intrinsics = [INTRINSICS_DICT[i] for i in current_cameras]
 # frame between robot and tag
-offset = np.array([1.09826201, -0.05144846999, 0.2276264]) 
+offset = np.array([1.09826201, -0.05144846999, 0.2276264])
 
 def coor_transform(tag_frame_pos):
     xt, yt, zt = tag_frame_pos
@@ -33,7 +31,6 @@ def get_mid_pos(markers_pos):
     detected = [np.linalg.norm(i) != 0 for i in positions]
     if not any(detected):
         return np.array([0, 0, 0])
-
     if all(detected):
         mid_pos = np.mean(positions, axis=0)
     elif all(detected[:2]):
@@ -86,7 +83,6 @@ def remove_datapoints_by_idx(info, ids):
         elif type(info[key]) is np.ndarray:
             info[key] = np.delete(info[key], ids, axis=0)
 
-
 if __name__ == "__main__":
     args = get_args()
     logs_folder = args.logs_folder + ('/' if args.logs_folder[-1] != '/' else '')
@@ -97,7 +93,6 @@ if __name__ == "__main__":
     logs_folder += '*/'
     name_of_demos = glob(logs_folder)
     print(f"Found {len(name_of_demos)} recordings in the {logs_folder}")
-
 
     if len(name_of_demos) == 0:
         exit()
@@ -112,129 +107,127 @@ if __name__ == "__main__":
         print("demo name: ", demo)
         try:
             csv_log = os.path.join(demo, 'log.csv')
-            csv_data = pd.read_csv(csv_log, names=['timestamp','robostate','robocmd', 'exec_cmd', 'goal', 'cam'])
+            csv_data = pd.read_csv(csv_log, names=['timestamp','robostate','robocmd', 'exec_command', 'goal', 'cam'])
         except Exception as e:
             print(f"Exception: cannot read log.csv at {demo}")
             continue
 
         initial_len = len(csv_data)
 
-        # Validates Camera Images
-        if not args.ignore_camera:
-            if type(csv_data['cam'][0]) == float:
-                import pdb; pdb.set_trace()
-            cam_ts = [_timestamps.split("-") for _timestamps in csv_data['cam']]
-            color_cam_fn = {CAMERA_IDS_DICT[i]:[] for i in current_cameras}
-            depth_cam_fn = {CAMERA_IDS_DICT[i]:[] for i in current_cameras}
-            for dp_idx, datapoint in enumerate(cam_ts):
-                my_image_fn = []
-                for j, cam_id in enumerate(current_cameras):
-                    i = CAMERA_IDS_DICT[cam_id]
-                    cimage_fn = f"c{i}-{cam_id}-{datapoint[j*2]}-color.jpeg"
-                    color_cam_fn[i].append(cimage_fn
-                        if os.path.isfile(os.path.join(demo, cimage_fn))
-                        else np.nan
-                    )
-                    if args.include_depth:
-                        dimage_fn = f"c{i}-{cam_id}-{datapoint[j*2+1]}-depth.jpeg"
-                        depth_cam_fn[i].append(dimage_fn
-                            if os.path.isfile(os.path.join(demo, dimage_fn))
+        try: 
+            # Validates Camera Images
+            if not args.ignore_camera:
+                if type(csv_data['cam'][0]) == float:
+                    import pdb; pdb.set_trace()
+                cam_ts = [_timestamps.split("-") for _timestamps in csv_data['cam']]
+                color_cam_fn = {CAMERA_IDS_DICT[i]:[] for i in current_cameras}
+                depth_cam_fn = {CAMERA_IDS_DICT[i]:[] for i in current_cameras}
+                for dp_idx, datapoint in enumerate(cam_ts):
+                    my_image_fn = []
+                    for j, cam_id in enumerate(current_cameras):
+                        i = CAMERA_IDS_DICT[cam_id]
+                        cimage_fn = f"c{i}-{cam_id}-{datapoint[j*2]}-color.jpeg"
+                        color_cam_fn[i].append(cimage_fn
+                            if os.path.isfile(os.path.join(demo, cimage_fn))
                             else np.nan
                         )
-            for cam_id in current_cameras:
-                i = CAMERA_IDS_DICT[cam_id]
-                csv_data[f"cam{i}c"] = color_cam_fn[i]
-                if args.include_depth:
-                    csv_data[f"cam{i}d"] = depth_cam_fn[i]
-
-            # Remove datapoints missing any image
-            csv_data.dropna(inplace=True)
-
-        valid_data = csv_data[csv_data['robocmd'] != 'None']
-        print(f"Found {initial_len} datapoints. " +
-            (f"{len(csv_data)} has all images. " if not args.ignore_camera else "") +
-            f"{len(valid_data)} usable. ")
-
-        # Extract info
-        jointstates = np.array([np.fromstring(_data, dtype=np.float64, sep=' ') for _data in valid_data['robostate']])
-        commands = np.array([np.fromstring(_data, dtype=np.float64, sep=' ') for _data in valid_data['exec_cmd']]) ### saving exec_cmd for some datasets!
-        goals = np.array([np.fromstring(_data, dtype=np.float64, sep=' ') for _data in valid_data['goal']])
-        info = {
-            'traj_id': os.path.basename(demo[:-1]),  # removing trailing /
-            'jointstates': jointstates,
-            'commands': commands,
-            'goals': goals,
-        }
-
-        if not args.ignore_camera:
-            for cam_id in current_cameras:
-                i = CAMERA_IDS_DICT[cam_id]
-                info[f"cam{i}c"] = list(valid_data[f"cam{i}c"])
-                if args.include_depth:
-                    info[f"cam{i}d"] = list(valid_data[f"cam{i}d"])
-        
-        no_marker_idx = []
-
-        mid_marker_positions = []
-        board_marker_positions = []
-        if not args.ignore_tracking:
-            for idx in MARKER_IDS:
-                info[f"marker{idx}"] = []
-
-            for i in range(len(info['jointstates'])):
-                imgs_ti = []
+                        if args.include_depth:
+                            dimage_fn = f"c{i}-{cam_id}-{datapoint[j*2+1]}-depth.jpeg"
+                            depth_cam_fn[i].append(dimage_fn
+                                if os.path.isfile(os.path.join(demo, dimage_fn))
+                                else np.nan
+                            )
                 for cam_id in current_cameras:
-                    _c = CAMERA_IDS_DICT[cam_id]
-                    img_fn = "{}/{}".format(info['traj_id'], info[f"cam{_c}c"][i])
-                    img = Image.open(os.path.join(args.logs_folder, img_fn)).copy()
-                    imgs_ti.append(np.array(img))
+                    i = CAMERA_IDS_DICT[cam_id]
+                    csv_data[f"cam{i}c"] = color_cam_fn[i]
+                    if args.include_depth:
+                        csv_data[f"cam{i}d"] = depth_cam_fn[i]
 
-                markers_pos_ti = get_markers_pos(imgs_ti, intrinsics=intrinsics, extrinsics=extrinsics, marker_ids=MARKER_IDS, marker_sizes=MARKER_SIZES)
+                # Remove datapoints missing any image
+                csv_data.dropna(inplace=True)
+
+            valid_data = csv_data[csv_data['robocmd'] != 'None']
+            print(f"Found {initial_len} datapoints. " +
+                (f"{len(csv_data)} has all images. " if not args.ignore_camera else "") +
+                f"{len(valid_data)} usable. ")
+
+            # Extract info
+            jointstates = np.array([np.fromstring(_data, dtype=np.float64, sep=' ') for _data in valid_data['robostate']])
+            commands = np.array([np.fromstring(_data, dtype=np.float64, sep=' ') for _data in valid_data['exec_command']]) ### saving exec_cmd for some datasets!
+            goals = np.array([np.fromstring(_data, dtype=np.float64, sep=' ') for _data in valid_data['goal']])
+            info = {
+                'traj_id': os.path.basename(demo[:-1]),  # removing trailing /
+                'jointstates': jointstates,
+                'commands': commands,
+                'goals': goals,
+            }
+
+            if not args.ignore_camera:
+                for cam_id in current_cameras:
+                    i = CAMERA_IDS_DICT[cam_id]
+                    info[f"cam{i}c"] = list(valid_data[f"cam{i}c"])
+                    if args.include_depth:
+                        info[f"cam{i}d"] = list(valid_data[f"cam{i}d"])
+            
+            no_marker_idx = []
+
+            mid_marker_positions = []
+            if not args.ignore_tracking:
                 for idx in MARKER_IDS:
-                    info[f"marker{idx}"].append(markers_pos_ti[idx])
+                    info[f"marker{idx}"] = []
 
-                mid_marker_pos = get_mid_pos(markers_pos_ti)
-                if np.linalg.norm(mid_marker_pos) == 0:
-                    no_marker_idx.append(i)
-                mid_marker_pos = coor_transform(mid_marker_pos)
-                board_marker_pos = markers_pos_ti[2] # not transformed
+                for i in range(len(info['jointstates'])):
+                    imgs_ti = []
+                    for cam_id in current_cameras:
+                        _c = CAMERA_IDS_DICT[cam_id]
+                        img_fn = "{}/{}".format(info['traj_id'], info[f"cam{_c}c"][i])
+                        img = Image.open(os.path.join(args.logs_folder, img_fn)).copy()
+                        imgs_ti.append(np.array(img))
 
-                if mid_marker_pos[1] < -0.5:
-                    import pdb; pdb.set_trace()
+                    markers_pos_ti = get_markers_pos(imgs_ti, intrinsics=intrinsics, extrinsics=extrinsics, marker_ids=MARKER_IDS, marker_sizes=MARKER_SIZES)
+                    for idx in MARKER_IDS:
+                        info[f"marker{idx}"].append(markers_pos_ti[idx])
 
-                mid_marker_positions.append(mid_marker_pos)
-                board_marker_positions.append(board_marker_pos)
-                cur_obs = np.concatenate([info['jointstates'][i], mid_marker_pos, info['goals'][i]])
+                    mid_marker_pos = get_mid_pos(markers_pos_ti)
+                    if np.linalg.norm(mid_marker_pos) == 0:
+                        no_marker_idx.append(i)
+                    mid_marker_pos = coor_transform(mid_marker_pos)
 
-        mid_marker_positions = np.vstack(mid_marker_positions)
+                    if mid_marker_pos[1] < -0.5:
+                        import pdb; pdb.set_trace()
+                    alpha = 0.25
+                    if len(mid_marker_positions) > 0:
+                        mid_marker_pos = mid_marker_pos * alpha + mid_marker_positions[-1] * (1 - alpha)
+                    mid_marker_positions.append(mid_marker_pos)
+                    cur_obs = np.concatenate([info['jointstates'][i], mid_marker_pos])
 
-        ############################ FOR MJRL ########################## 
-        #### not post processing goals, use the original scripted goals
-        print(mid_marker_positions[0], info['goals'][0])
-        info['observations'] = np.concatenate([info['jointstates'], mid_marker_positions, info['goals']], axis=1)
-        info['actions'] = info['commands']
-        termination = np.zeros(len(info['commands']))
-        termination[-1] = 1
-        info['terminated'] = termination
-        print("no marker idx: ", no_marker_idx)
-        remove_datapoints_by_idx(info, no_marker_idx)
+            mid_marker_positions = np.vstack(mid_marker_positions)
 
-        from rewards.lifting import reward_function # CHOOSE THE TASK
-        tmp_info = {}
-        tmp_info["observations"] = np.expand_dims(info["observations"], axis=0)
-        tmp_info["actions"] = np.expand_dims(info["actions"], axis=0)
-        reward_function(tmp_info)
-        info['rewards'] = tmp_info['rewards'][0]
-        success = np.linalg.norm(board_marker_positions[-1]) == 0
-        success = False
-        if success:
-            best_rewards.append(1)
-        else:
+            ############################ FOR MJRL ########################## 
+            #### not post processing goals, use the original scripted goals
+            info['observations'] = np.concatenate([info['jointstates'], mid_marker_positions], axis=1)
+            info['actions'] = info['commands']
+            termination = np.zeros(len(info['commands']))
+            termination[-1] = 1
+            info['terminated'] = termination
+            print("no marker idx: ", no_marker_idx)
+            remove_datapoints_by_idx(info, no_marker_idx)
+
+            from rewards.grasping import reward_function # CHOOSE THE TASK
+            tmp_info = {}
+            tmp_info["observations"] = np.expand_dims(info["observations"], axis=0)
+            tmp_info["actions"] = np.expand_dims(info["actions"], axis=0)
+            reward_function(tmp_info)
+            info['rewards'] = tmp_info['rewards'][0]
             best_rewards.append(max(info['rewards']))
-        import matplotlib.pyplot as plt
-        ######################################################
-        list_of_demos.append(info)
-        count_dp += len(valid_data)
+            print("max_reward: ", max(info['rewards']), "   last reward: ", info['rewards'][-1])
+            import matplotlib.pyplot as plt
+            ######################################################
+            list_of_demos.append(info)
+            count_dp += len(valid_data)
+        except Exception as e:
+            print(e)
+            continue
     print(best_rewards, np.mean(best_rewards), np.std(best_rewards))
     with open(args.output_pickle_name, 'wb') as f:
         pickle.dump(list_of_demos, f)
